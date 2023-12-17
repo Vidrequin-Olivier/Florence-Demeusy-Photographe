0d4b0dcb93d3b90f4b5e5af08007f4e29630a319
Vidrequin Olivier <vidrequin.olivier@gmail.com>
2023-12-17 12:42:52 +0100

    divers correctifs mineurs

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ let coupleHalloween = {
    id: "ça tu le laisse, c'est le code qui le gère",
    img: "couple-halloween .webp",
    ajust: `50% 90%`,
-   alt: `Photo de deux personnages en salopette avec des têtes en forme de citrouilles d'Halloween, dans une forêt automnale.`,
+   alt: `Photo de deux personnes en salopette et chemise à carreaux avec des têtes en forme de citrouilles d'Halloween, dans une forêt automnale.`,
    figcaption: `Couple Halloween !`,
    p: `Aaaahh cette séance totalement spontané figure par une de mes préférés de ces temps ci ! C’était fun, c’était spooky, c’était mignon, bref, j’ai adorée !! L’année prochaine j’espère vous voir bien plus nombreux à sauter le pas pour faire ce type de séance !! Vous allez pas être déçu du résultat je vous le… `,
    a: `https://florencedemeusy.fr/2023/11/09/couple-halloween/`
@@ -23,7 +23,7 @@ let babylloween = {
    id: "ça tu le laisse, c'est le code qui le gère",
    img: "babylloween.webp",
    ajust: ``,
-   alt: `Un bébé souriant habillé en petit paysan trop mignon, dans un décors de grange à paille avec des citrouilles d'Halloween.`,
+   alt: `Un bébé souriant habillé en petit épouvantail trop mignon, dans un décors de grange à paille avec des citrouilles d'Halloween.`,
    figcaption: `Babylloween`,
    p: `Voici ma séance baby épouvantail ultra mignonne suivis de quelques clichés fantômes de cette période d’Halloween. Merci à Mélodie et Steve de m’autoriser à publier les images de leur petit tout Evan ! La spooky season étant terminé, j’ai déjà hâte la prochaine !`,
    a: `https://florencedemeusy.fr/2023/11/08/babylloween/`
@@ -39,22 +39,15 @@ let etienne = {
    a: "https://florencedemeusy.fr/2023/07/14/etienne/"
 };
 
-// liste des projets:
+// liste des projets affichés:
 const listeProjets = [
    witchyWeen, coupleHalloween, babylloween, etienne
 ];
 
-/*
-travail à faire:
-   - définir un nombre de caratères max pour la boite .works article p et faire que, si le texte dépasse, il est tronqué et ça affiche 3 points de suspension.
-      une fois que c'est fait, copier l'intégralité du contenus des paragraphes (pour Couple Halloween et pour Etienne) depuis leur lien et remplacer le contenus actuel de leurs p.
-   - re-créer les liens entre le css et les éléments dans works pour la police d'écriture.
-*/
-
 // change l'id de l'objet en fonction de sa position dans le tableau
 function idDefiner(listeProjets) {
    listeProjets.forEach((projet, index) => {
-      projet.id = index + 1;
+      projet.id = index;
    });
 };
 
@@ -70,6 +63,7 @@ function addWorks(listeProjets) {
       const img = document.createElement("img");
       const figcaption = document.createElement("figcaption");
       const p = document.createElement("p");
+      const pa = document.createElement("p");
       const aReadMore = document.createElement("a");
 
       aImg.href = projet.a;
@@ -88,24 +82,25 @@ function addWorks(listeProjets) {
                <figure>
                   <a><img></a>
                   <figcaption></figcaption>
-                  <p>texte</p>
-                  <a>Lire la suite</a>
                </figure>
+               <p>texte</p>
+               <p><a>Lire la suite</a></p>
             </article>
          </li>
       */
       aImg.appendChild(img);
       figure.appendChild(aImg);
       figure.appendChild(figcaption);
-      figure.appendChild(p);
       aReadMore.innerHTML= "Lire la suite";
-      figure.appendChild(aReadMore);
+      pa.appendChild(aReadMore);
       article.appendChild(figure);
+      article.appendChild(p);
+      article.appendChild(pa);
       li.appendChild(article);
       works.appendChild(li);
 
       // définit une marge d'un coté ou de l'autre de l'article selon si il est placé à gauche ou à droite sur les grands écrans.
-      article.className = ((projet.id % 2 === 0) ? "rightArticle" : "leftArticle");
+      article.className = ((projet.id % 2 === 0) ? "leftArticle" : "rightArticle");
    });
 };
 
